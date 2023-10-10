b8ba424bbe3087b7b4344905934d36543615a52d
Third rate coder <cpdlxkr23@gmail.com>
2023-10-10 22:29:58 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [title, setTitle] = useState(["남자 코트 추천", "강남 우동 맛집", "파이썬독학"]);
+	let [like, setLike] = useState(0);
+
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <h4>ReactBlog</h4>
+            </div>
+			<button onClick={ () => {
+				let copyTitle = [...title];
+				copyTitle.sort();
+				setTitle(copyTitle);
+				}}>가나다순정렬</button>
+            <div className="list">
+                <h4>{ title[0] } <span onClick={ () => { setLike(like+1); }}>👍</span> { like } </h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className="list">
+                <h4>{ title[1] }</h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className="list">
+                <h4>{ title[2] }</h4>
+                <p>2월 17일 발행</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;

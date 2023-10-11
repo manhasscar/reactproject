cd594c69d723543c65ec05c397d646b70e54aeb9
Third rate coder <cpdlxkr23@gmail.com>
2023-10-11 23:37:31 +0900

    reacttrain

--- src/App.js
+++ src/App.js
@@ -2,31 +2,68 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-    let [title, setTitle] = useState(["남자 코트 추천", "강남 우동 맛집", "파이썬독학"]);
-	let [like, setLike] = useState(0);
-
+    let [titles, setTitles] = useState(["남자 코트 추천", "강남 우동 맛집", "파이썬독학"]);
+	let [like, setLike] = useState([0, 0, 0]);
+    let [titleNum, setTitleNum] = useState(0);
+    let [isModalOn, setIsModalOn] = useState(false);
+    let [input, setInput] = useState('');
     return (
         <div className="App">
             <div className="black-nav">
                 <h4>ReactBlog</h4>
             </div>
-			<button onClick={ () => {
-				let copyTitle = [...title];
-				copyTitle.sort();
-				setTitle(copyTitle);
-				}}>가나다순정렬</button>
-            <div className="list">
-                <h4>{ title[0] } <span onClick={ () => { setLike(like+1); }}>👍</span> { like } </h4>
-                <p>2월 17일 발행</p>
-            </div>
-            <div className="list">
-                <h4>{ title[1] }</h4>
-                <p>2월 17일 발행</p>
-            </div>
-            <div className="list">
-                <h4>{ title[2] }</h4>
-                <p>2월 17일 발행</p>
-            </div>
+
+            {
+                titles.map((title, i) => {
+                    return (
+                    <div className="list" key={i}> 
+                        <h4 onClick={ () => { 
+                            setTitleNum(i); 
+                            isModalOn === true ? setIsModalOn(false) : setIsModalOn(true); 
+                            } }>
+                            { title } 
+                            <span onClick={(e) => {
+                                e.stopPropagation();
+                                let copyLike = [...like];
+                                copyLike[i] = copyLike[i] + 1;
+                                setLike(copyLike); }
+                                }>
+                                👍
+                            </span> 
+                            { like[i] }
+                        
+                        </h4>                   
+                        <p>2월 17일 발행</p>
+                        <button onClick={(e) => {
+                            e.stopPropagation();
+                            let copyTitels = [...titles]; 
+                            copyTitels.splice(i,1); 
+                            setTitles(copyTitels)
+                            }}>
+                            글 삭제
+                        </button>
+                    </div>
+                    );
+                })
+            }
+
+            {
+                isModalOn === true ? <Modal titles={titles} titleNum={titleNum} ></Modal> : null
+            }
+
+            <input onChange={(e) => { setInput(e.target.value) }}></input>
+            <button onClick={() => {let copyTitels = [...titles]; copyTitels.push(input); setTitles(copyTitels)}}>추가</button>
+        </div>
+    );
+}
+
+const Modal = (props) => {
+    return (
+        <div className="modal">
+            <h4>{ props.titles[props.titleNum] }</h4>
+            <p>날짜</p>
+            <p>상세내용</p>
+            <button>글수정</button>
         </div>
     );
 }
